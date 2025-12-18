bb0c7436e46e5b939ddcae70f040576d3e689c91
maxihan1115-coder <maxihan1115-coder@github.com>
2025-12-18 18:38:40 +0900

    단지 버그 수정

--- src/app/404.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ minHeight: '60vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <h1 style={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '1rem' }}>페이지를 찾을 수 없습니다.</h1>
-      <p style={{ color: '#888' }}>요청하신 페이지가 존재하지 않거나, 이동되었을 수 있습니다.</p>
-    </div>
-  );
-} 
